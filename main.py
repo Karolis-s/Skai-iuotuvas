d425351c2344ea9666767fa4bf76418bbc3f70da
MantasZup123 <mantaszup@gmail.com>
2024-11-11 21:23:45 +0200

    added an if function to check the number

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ class Calculator:
     def __init__(self):
         pass
 
-    def subtract(self, a, b):
-        return a - b
+    def subtract(self, a: float, b: float) -> float:
+        if self.number_validation(a) and self.number_validation(b):
+            return a - b
+        else:
+            return 'Wrong Input'
 
     def addition(self, a: float , b: float) -> float:
         if self.number_validation(a) and self.number_validation(b):
